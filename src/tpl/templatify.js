d0a725ad0a6aa48aa1b1606e3df517574ec210b7
Matthias Leuffen <matthes@leuffen.de>
2022-12-02 00:42:02 +0100

    fix quote

--- src/tpl/templatify.js
+++ src/tpl/templatify.js
@@ -1,6 +1,23 @@
 import {ka_query_selector} from "../core/query-select.js";
 import {ka_elwalk} from "../core/elwalk.js";
 
+function quoteattr(s, preserveCR) {
+    preserveCR = preserveCR ? '&#13;' : '\n';
+    return ('' + s) /* Forces the conversion to string. */
+        .replace(/&/g, '&amp;') /* This MUST be the 1st replacement. */
+        .replace(/'/g, '&apos;') /* The 4 other predefined entities, required. */
+        .replace(/"/g, '&quot;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        /*
+        You may add other replacements here for HTML only
+        (but it's not necessary).
+        Or for XML, only if the named entities are defined in its DTD.
+        */
+        .replace(/\r\n/g, preserveCR) /* Must be before the next replacement. */
+        .replace(/[\r\n]/g, preserveCR);
+    ;
+}
 
 window._ka_el_idx = 0;
 /**
@@ -22,7 +39,7 @@ export function ka_templatify (elem, returnMode=true) {
         returnTpl.setAttribute("_kaidx", (window._ka_el_idx++).toString())
         /* @var {HTMLTemplateElement} returnTpl */
         returnTpl.innerHTML = elem.innerHTML
-            .replace(/\[\[(.*?)\]\]/g, (matches, m1) => `<span ka.textContent="${m1}"></span>`);
+            .replace(/\[\[(.*?)\]\]/g, (matches, m1) => `<span ka.textContent="${quoteattr(m1)}"></span>`);
 
         ka_templatify(returnTpl.content, false);
         return returnTpl;
