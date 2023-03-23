e123c56ee0e1755dee3da81732faa7648e699276
Matthias Leuffen <matthes@leuffen.de>
2023-03-23 18:40:14 +0100

    fix create

--- src/core/create-element.ts
+++ src/core/create-element.ts
@@ -8,7 +8,7 @@
  * @param children {HTMLElement[]}
  * @return HTMLElement
  */
-export function ka_create_element (tagName : string, attributes : any = null,  children : HTMLElement[] | NodeListOf<Node> = null, appendToElement : HTMLElement = null) : HTMLElement{
+export function ka_create_element (tagName : string, attributes : any = null,  children : HTMLElement[] | NodeList = null, appendToElement : HTMLElement = null) : HTMLElement{
     let e = document.createElement(tagName);
     if (attributes === null)
         attributes = {}
@@ -17,9 +17,14 @@ export function ka_create_element (tagName : string, attributes : any = null,  c
         e.setAttribute(attName, attributes[attName]);
     }
 
-    if (Array.isArray(children) || children instanceof NodeList) {
-        for(let ce of children)
+    if (children instanceof NodeList) {
+        children = Array.from(children) as HTMLElement[];
+    }
+
+    if (Array.isArray(children)) {
+        for(let ce of children) {
             e.appendChild(ce);
+        }
     }
 
     if (appendToElement !== null) {
