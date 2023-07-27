514b7759ca59d7739503cefa5c4a8832add87701
Matthias Leuffen <matthes@leuffen.de>
2023-07-27 15:55:10 +0200

    update

--- src/core/query-select.ts
+++ src/core/query-select.ts
@@ -8,7 +8,7 @@
  * @return {HTMLElement}
  */
 export function ka_query_selector (query : string, parent : HTMLElement | Document | DocumentFragment  = null, exception : any = null) : HTMLElement{
-    if (typeof exception === "undefined")
+    if (typeof exception === "undefined" || exception === null)
         exception = `querySelector '${query}' not found`
     if (typeof parent === "undefined" || parent === null)
         parent = document;
