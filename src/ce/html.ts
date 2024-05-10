50e210817c3a2ced9e8be04a8472ab4b4214de9c
Matthias Leuffen <matthes@leuffen.de>
2024-05-10 13:48:04 +0200

    sync

--- src/ce/html.ts
+++ src/ce/html.ts
@@ -1,9 +1,27 @@
 
+
+function returnFirstOriginalLineThatIsDifferent(original : string, generated : string) : string
+{
+    let originalLines = original.split("\n");
+    let generatedLines = generated.split("\n");
+
+    for (let i = 0; i < originalLines.length; i++) {
+        if (originalLines[i] !== generatedLines[i]) {
+            return "Line `" + (i + 1) + ": " + originalLines[i].trim() + "`";
+        }
+    }
+    return null;
+}
+
 export function ka_html(htmlContent : string | HTMLTemplateElement) : HTMLTemplateElement {
     if (htmlContent instanceof HTMLTemplateElement) {
         return htmlContent;
     }
     let e = document.createElement("template");
+
     e.innerHTML = htmlContent;
+    if (e.innerHTML !== htmlContent)
+        console.error("ka_html(): HTML Parsing Error (possibly due to invalid HTML in template) in " + returnFirstOriginalLineThatIsDifferent(htmlContent, e.innerHTML), htmlContent);
+
     return e;
 }
